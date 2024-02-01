2727d0175bea132c09b5ed74890e1b1edf17a5d3
SH_SOHEL <sujauddoulasohel352@gmail.com>
2024-02-02 05:34:07 +0600

    adde new tasket

--- arrayTaske1.js
+++ arrayTaske1.js
@@ -3,6 +3,7 @@
 // 2. console log the 3rd index element
 // 3. change the value of the 2nd index element to jambura
 // 4. console log the final array
+/** Array Items Call and Chang */
 const fruits=['Mango','Banana','Apple','water Melon','Origen'];
 console.log(fruits[3]);
 fruits[2]='jambura';
